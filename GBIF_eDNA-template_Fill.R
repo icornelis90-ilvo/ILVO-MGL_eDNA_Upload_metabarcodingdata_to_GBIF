526f21788685268e04a3d8238f9d895b8d6716c6
Isolde Cornelis <156781000+icornerlis@users.noreply.github.com>
2025-07-03 15:28:53 +0200

    Add files via upload

---
+++ GBIF_eDNA-template_Fill.R
@@ -0,0 +1,152 @@
+###### Isolde Cornelis
+###### 11/04/2024
+###### Create the template file to upload data to the from eDNA metabarcoding to GBIF
+
+#!/usr/bin/env Rscript
+if(.Platform$OS.type == "unix"){home<-"/home/"} else{
+  home<-"//192.168.236.131/"}
+
+library(vegan)
+library(seqRFLP)
+library(dplyr)
+library(tibble)
+library(stringr)
+library(here)
+
+### make paths
+proj.path.GBIF <- here("/home/genomics/icornelis/07_GBIF/02_Uploading Files/")
+proj.path.data <- here("/home/genomics/icornelis/07_GBIF/03_Input Files/")
+
+### upload data
+## OTU-tables and taxa (standard files created by the dada2 pipeline)
+OTU_table <- readxl::read_excel(paste0(proj.path.data,"table_unrarefied_concatenated_CleanedASVs_WithField_FullTaxonomicAssignment.xlsx"))
+ASVs <- read.table(paste0(proj.path.data,"asvs.tsv"), header = T)
+
+## samples
+#The MIMARCKS file created to upload data to NCBI
+Attributes_Full <- readxl::read_excel(paste0(proj.path.data,"Attributes_NJ2021_12S.xlsx"))
+#file to be downloaded from bioproject through "download metadata file with SRA accessions"
+SRR <- readxl::read_excel(paste0(proj.path.data,"SRRcodes_NJ2021_12S.xlsx"))
+
+###Get data
+##OTU_table
+OTU <- OTU_table %>%
+  #Remove columns with information about the taxonomic classification
+  select(!Kingdom:Comment) %>% 
+  #rename the column ASV to id
+  rename(id = ASV) %>%
+  #remove columns from negative control samples
+  select(!contains("neg"))
+
+##taxa
+taxa <- OTU_table %>%
+  #Select columns with information about the taxonomic classification
+  select(ASV:Comment) %>%  
+  rename(id = ASV, #rename the column ASV to id
+         scientificName = TaxonomicAssignment) %>% #rename the column TaxonomicAssignment to scientificName
+  #group_by id to consider each group separately 
+  group_by(id) %>%
+  #add the ASV sequences to the taxonomic information
+  mutate(DNA_sequence = ASVs$dnas[which(ASVs$names== id)], 
+         len = ASVs$len[which(ASVs$names==id)],
+         asv = ASVs$asv[which(ASVs$names==id)]) %>% 
+  ungroup() %>%
+  #move the column "asv" to the first position
+  select(asv, everything()) %>% 
+  #move the columns "DNA_sequence" and "len" before the column "Kingdom"
+  relocate(c(DNA_sequence, len), .before = Kingdom)
+
+##samples
+SRR <- SRR %>%
+  #create a new column "*sample_name" containing the file name without the extension _1.bz
+  mutate(`*sample_name` = str_replace(SRR$filename, "_1.bz2", ""))
+
+Attributes <- Attributes_Full %>%
+  #add colnames to the Attribute table (row 11)
+  scrutiny::row_to_colnames(11) %>%
+  #remove the heading of the Attribute table (first 11 rows)
+  filter(!is.na(sample_title)) %>%
+  #remove empty columns from the Attribute table
+  select_if(function(x) !(all(is.na(x)))) %>%
+  #change the colname of *collection_date to "eventDate"
+  rename(eventDate = `*collection_date`) %>%
+  #group_by *sample_name to consider each name separately
+  group_by(`*sample_name`) %>%
+  #Add the SRR and biosample accession numbers from NCBI to the attributes table
+  mutate(materialSampleID = ifelse(`*sample_name` %in% SRR$`*sample_name`,
+                                   SRR$biosample_accession[which(SRR$`*sample_name`==`*sample_name`)],
+                                   ""),
+         SRR_accession = ifelse(`*sample_name` %in% SRR$`*sample_name`,
+                               SRR$accession[which(SRR$`*sample_name`==`*sample_name`)],
+                               "")) %>%
+  ungroup() %>%
+  mutate(SRR_link = ifelse(SRR_accession == "", 
+                           "",
+                           paste("https://www.ncbi.nlm.nih.gov/sra/?term=",
+                                 SRR_accession,
+                                 sep="")), #add ncbi link to the reads
+         SAMN_link = ifelse(materialSampleID == "", 
+                            "", 
+                            paste("https://www.ncbi.nlm.nih.gov/biosample/", 
+                                  materialSampleID, 
+                                  sep="")), #add ncbi link to the samples
+         #add Latitude
+         decimalLatitude = ifelse(`*lat_lon` == "not applicable",
+                                  `*lat_lon`, stringr::word(`*lat_lon`, 1)),
+         #add Longitude
+         decimalLongitude = ifelse(`*lat_lon` == "not applicable",
+                                   `*lat_lon`, stringr::word(`*lat_lon`, 3)))
+
+#create table new table to add the information for the samples sheet
+samples <- Attributes %>%
+  #keep only the columns of interest from the Attributes table
+  select(sample_title, eventDate, decimalLatitude, decimalLongitude,
+         SRR_link, SAMN_link) %>%
+  #rename the column sample_title to id
+  rename(id = sample_title) %>%
+  #keep only unique rows, this removes double ids due to the PCR replicates
+  #distinct() %>%
+  #filter to keep only the samples present in the OTU table 
+  filter(., id %in% colnames(OTU)) %>%
+  #group by the columns with same informatie for each PCR replicate
+  group_by(id, eventDate, decimalLatitude, decimalLongitude) %>%
+  #summarise the data for the ncbi links for the PCR replicates per samples and separate them by |
+  reframe(associatedSequences = paste(SRR_link, collapse = " | "),
+          materialSampleID = paste(SAMN_link, collapse = " | ")) %>%
+  #for some samples the sequences were not uploaded to NCBI (unused in the paper)
+  #leave the columns "associatedSequences" and "materialSampleID" empty
+  mutate(associatedSequences = ifelse(associatedSequences == " |  | ", 
+                                      "", 
+                                      associatedSequences),
+         materialSampleID = ifelse(materialSampleID == " |  | ", 
+                                    "",
+                                    materialSampleID))
+
+##defaultValue
+defaultValue <- data.frame(
+  #add the terms that need to be filled in in GBIF
+  term = c("env_medium",
+           "target_gene",
+           "pcr_primer_forward",
+           "pcr_primer_name_forward",
+           "pcr_primer_reverse",
+           "pcr_primer_name_reverse",
+           "seq_meth",
+           "otu_db"),
+  #add the values to be filled in for each term (adjust where needed for your data)
+  value = c("seawater",
+            "12S",
+            "5’-GT(C/T)GGTAAA(A/T)CTCGTGCCAGC-3’",
+            "MiFish_U/E_F",
+            "5’-CATAGTGGGGTATCTAATCC(C/T)AGTTTG-3’",
+            "MiFish_U/E_R",
+            "Illumina MiSeq",
+            "custom made reference database"))
+
+### Save the data into one file
+list_of_datasets <- list("OTUtable" = OTU,
+                         "taxa" = taxa,
+                         "samples" = samples,
+                         "defaultValue" = defaultValue)
+openxlsx::write.xlsx(list_of_datasets, paste0(proj.path.GBIF,"/edna_template_filled.xlsx"), colNames = T)
+
---
+++ README.md
@@ -0,0 +1,43 @@
+# eDNA_metabarcoding_Upload-to-GBIF
+
+## Description
+This script allows you to fill in the template file, in excel format, needed to upload the eDNA metabarcoding data to GBIF (https://edna-tool.gbif-uat.org/).
+The template file consists of 4 sheets:
+* Sheet1 = OTUtable\
+The OTU table each row must represent a unique OTU, and each column must represent a different sample or site, with the cell values indicating the count of sequencing reads of the OTUs in each sample. The OTU table will be linked to the two other tables
+* Sheet2 = taxa\
+The taxa table relates to the OTUs and must contain at least the sequence, but likely also the taxonomic classification. The order of the OTUs in the OTU- and taxa table must be the same
+* Sheet3 = samples\
+The sample table contains the metadata relating to the samples/sites. The order of the samples must in the OTU-table (columns) and samples-table (rows) must be the same
+* Sheet4 = defaultValue\
+Add the values you want to add in GBIF, such as target sequence, used primers and sequencing method, etc
+
+## Needed input files
+1. OTU_table:\
+ASV table containing the unrarefied and concatenated data after decontamination of the contaminant ASVs\
+Rows are the ASVs and the Columns are the samples\
+The last 11 columns contain the taxonomic classification, ASV-name, and the results after taxonomic assignment with DADA2 and BLASTn against the costumn reference database and BLASTn against GenBank
+2. ASVs:\
+Table, generated while running DADA2, that contains all the:\
+&emsp; - ASV-names (names)\
+&emsp; - ASV-sequences (dnas)\
+&emsp; - ASV-lengths (len)
+3. Attributes_Full:\
+The complete MIMARCKS file created to upload the sequencing data to NCBI
+4. SRR:\
+The metadata file containing the SRA accessions, which can be downloaded from the bioproject on NCBI
+
+## Steps
+1. Create the OTU_table (Sheet1):\
+The script uses the ASVs from the unrarefied, concatenated and cleaned ASV-table. The last 11 cloumns with the taxonomic classification and all negative control samples will be removed. ASVs without reads will be removed from the ASV-table.
+2. Create the taxa-table (Sheet2):\
+The script uses the last 11 columns of the ASV-table that contain the taxonomic classification. The columns "Species" and "ASV" are renamed to "scientificName" and "id", in order to be automatically recognized by GBIF. The column "id" is moved to the first position. Afterwards the ASV-sequence and length are added before the column "Kingdom"
+3. Create the samples-table (Sheet3):\
+The script uses the Attributes table to add the metadata to each sample. In addition, the links to the sequencing file (associatedSequence) and sample metadata (materialSampleID) on SRA are added. Since the concatenated samples are used, the links for all three PCR/technical replicates, separated by "|", are added for each sample
+4. Create the defaultValue-table (Sheet4):\
+The default values are added manually to the table, through the lists term and value.
+5. Save the file:\
+All four tables are saved as seperate sheets in on file in Excel-format (xlsx). 
+
+## Upload to GBIF
+Use the created template file to upload the eDNA metabarcoding data to https://edna-tool.gbif-uat.org/. More information about uploading the data can be found on: https://docs.gbif-uat.org/edna-tool-guide/en
